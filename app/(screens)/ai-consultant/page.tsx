0453b20be31c8e6b4dc1df18e57cfd297ab2ac34
utkarshOjha2001 <utkarsh.ojha@audaxlabs.com>
2026-05-11 17:30:52 +0530

    Updates on Chatsection

--- app/(screens)/ai-consultant/page.tsx
+++ app/(screens)/ai-consultant/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 import React from 'react'
 import HistoryTab from '@/components/HistoryTab';
+import AIChatBox from '@/components/AIChatBox';
 
 const AiConsutant = () => {
   return (
-    <div className='p-4'>
+    <div className='pl-[15%] pr-[15%] w-full h-full flex flex-col gap-4'>
+      <AIChatBox />
     </div>
     
   )
---
+++ components/AIChatBox.tsx
@@ -0,0 +1,77 @@
+'use client';
+import React from 'react'
+import { SendHorizonal } from 'lucide-react';
+
+const AIChatBox = () => {
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        alert('submit');
+    }
+    return (
+        <form onSubmit={handleSubmit}
+            className='absolute bottom-20 right-[14%] z-50  flex h-20 rounded-xl w-[50%] gap-4 bg-[#0d1c2d] p-2 overflow-y-auto scrollbar-thin'
+        >
+            <textarea
+                placeholder="Ask anything..."
+                className="
+    w-full
+    h-full
+
+    resize-none
+    outline-none
+    border-none
+    ring-0
+    focus:outline-none
+    focus:ring-0
+    focus:border-none
+
+    bg-transparent
+
+    text-[#E4E4E7]
+    placeholder:text-[#71717A]
+
+    text-md
+    font-normal
+
+    px-2
+    py-2
+
+    scrollbar-thin
+  "
+            />
+            <button
+                type='submit'
+                className="
+    cursor-pointer
+    flex
+    items-center
+    justify-center
+
+    rounded-xl
+
+    px-2
+    min-w-14
+    h-[80%]
+    bg-linear-to-br
+    from-[#7B6DFF]
+    via-[#8B5CF6]
+    to-[#5D5FEF]
+
+    shadow-[0_0_20px_rgba(123,109,255,0.45)]
+
+    transition-all
+    duration-300
+
+    hover:scale-100
+    hover:shadow-[0_0_28px_rgba(123,109,255,0.7)]
+
+    active:scale-95
+  "
+            >
+                <SendHorizonal className="w-5 h-5 text-white" />
+            </button>
+        </form>
+    );
+};
+
+export default AIChatBox;
